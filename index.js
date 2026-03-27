5376bf20c742a706ee20d58b699a60a886e12c6e
sanjeev23oct <65849546+sanjeev23oct@users.noreply.github.com>
2026-03-27 15:26:03 +0530

    Update response message in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World from Berth!\n')
+  res.end('Hello World from Berth again!\n')
 })
 server.listen(process.env.PORT || 3000, () => {
   console.log('Server running on port', process.env.PORT || 3000)
